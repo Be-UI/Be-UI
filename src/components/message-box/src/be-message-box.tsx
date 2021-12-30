6d03f01195505dd36b0ee04f1baaa595f44e6224
czh740132583 <740132583@qq.com>
2021-12-30 14:07:31 +0800

    1.message-box 确认取消close回调

--- src/components/message-box/src/be-message-box.tsx
+++ src/components/message-box/src/be-message-box.tsx
@@ -127,7 +127,6 @@ export default defineComponent({
          * 关闭方法，销毁组件
          */
         const close = (): void => {
-            show.value = false
             /** close事件
              * @event close
              */
@@ -139,7 +138,7 @@ export default defineComponent({
         const confirmFunc = (): void => {
             // this.$selfEvent.confirm && this.$selfEvent.confirm()
             props.onConfirm && props.onConfirm()
-            close()
+            show.value = false
         }
         /************************************** 监听设置遮罩 ******************************/
         const dialogModels = ref('')
