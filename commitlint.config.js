1bd7ec7ae62f08e5862e48972ce4dfa6ca311aab
czh740132583 <740132583@qq.com>
2022-03-07 17:13:23 +0800

    feat: 添加测试commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,11 @@
+// 已经按照官网进行配置 但是不生效，使用webstorm插件作为本地校验吧
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [
       2,
       'always',
-      ['feats', 'fix', 'doc', 'style', 'refactor', 'test', 'revert', 'build', 'ci', 'perf', 'conf']
+      ['feat', 'fix', 'doc', 'style', 'refactor', 'test', 'revert', 'build', 'ci', 'perf', 'conf']
     ]
   }
 };
