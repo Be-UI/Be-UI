b4badee2c3255c0e5bca4c65ba8d502307d96662
czh740132583 <740132583@qq.com>
2022-04-27 13:42:36 +0800

    chore: 暂时更换到windicss

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import dts from 'vite-dts'
+import WindiCSS from 'vite-plugin-windicss'
 import cleanup from 'rollup-plugin-cleanup'
 import * as path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), dts(), cleanup()],
+  plugins: [vue(), vueJsx(), dts(), cleanup(), WindiCSS()],
 
   /**
    * 在生产中服务时的基本公共路径。
