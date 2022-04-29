6132140420aaa6f33080327d65974ebabb79cff4
czh740132583 <740132583@qq.com>
2022-04-29 14:29:03 +0800

    style: 修复 input-number样式

--- docs/.vitepress/plugin/plugins.ts
+++ docs/.vitepress/plugin/plugins.ts
@@ -2,7 +2,6 @@ import path from 'path'
 import fs from 'fs'
 import MarkdownIt from 'markdown-it'
 import mdContainer from 'markdown-it-container'
-// import { highlight } from '../utils/highlight'
 import type Token from 'markdown-it/lib/token'
 import type Renderer from 'markdown-it/lib/renderer'
 import {highlight} from "../utils/highlight";
