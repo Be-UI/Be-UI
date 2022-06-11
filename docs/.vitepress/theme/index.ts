9f15a8e7aaf7809aafacc4157f6d1c67ba29df67
czh740132583 <740132583@qq.com>
2022-06-11 22:23:10 +0800

    chore: 打包还差删除子包

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,13 +3,13 @@ import 'normalize.css'
 import index, { globals } from '../vitepress'
 import type { Theme } from 'vitepress'
 import 'virtual:windi.css'
-import '../../../dist/css/style.css'
-import BeUI from '../../../dist/lib/be-ui.es.js'
+import '../../../dist/style/css/index.css'
+//import BeUI from '../../../dist/index.esm.js'
 export const define = <T>(value: T): T => value
 export default define<Theme>({
   Layout: index,
   enhanceApp: ({ app }) => {
-    app.use(BeUI)
+   // app.use(BeUI)
     globals.forEach(([name, Comp]) => {
       app.component(name, Comp)
     })
