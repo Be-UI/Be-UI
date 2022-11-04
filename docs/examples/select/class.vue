a7205806869ceb6084646824523f51788442abb8
chenzhihui <18281682921@qqcom>
2022-11-04 16:30:41 +0800

    docs: adde select component document

---
+++ docs/examples/select/class.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="exp-input">
+    <be-select size="medium" :list="baseList"
+               keyValue="id"
+               customClass="test-select"
+               v-model="value"
+               labelValue="label">
+    </be-select>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+const baseList = ref<Array<{ id: string, label: string }>>([
+  {id: '君埋泉下泥销骨', label: '君埋泉下泥销骨'},
+  {id: '我寄人间雪满头', label: '我寄人间雪满头'},
+  {id: '满堂花醉三千客', label: '满堂花醉三千客'},
+  {id: '一剑霜寒十四州', label: '一剑霜寒十四州'},
+  {id: '两人对酌山花开', label: '两人对酌山花开'},
+  {id: '一杯一杯复一杯', label: '一杯一杯复一杯'},
+  {id: '我醉欲眠卿且去', label: '我醉欲眠卿且去'},
+  {id: '明朝有意抱琴来', label: '明朝有意抱琴来'},
+])
+const value = ref<string>('一剑霜寒十四州')
+</script>
+<style lang="scss">
+.exp-input{
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  .be-select .be-select--body.test-select{
+
+    border:1px solid #2aad1f;
+
+  }
+}
+</style>
---
+++ docs/examples/select/custom.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="exp-input">
+    <be-select size="medium"
+               keyValue="id"
+               group
+               :list="groupList"
+               v-model="value"
+               labelValue="label">
+      <template v-slot="slotProps">
+        <span >😔{{ slotProps.label }} ❥</span>
+      </template>
+    </be-select>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+const value = ref<string>('我寄人间雪满头')
+const groupList = ref<Array<{ id: string, label: string ,type?: string ,children?:Array<any>}>>([
+  {id: '分组一', label: '分组一',type:'group',children:[{id: '君埋泉下泥销骨', label: '君埋泉下泥销骨'}]},
+  {id: '分组二', label: '分组二',type:'group',children:[{id: '我寄人间雪满头', label: '我寄人间雪满头'}]},
+])
+</script>
+<style lang="scss">
+.exp-input{
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  .be-select{
+    width: 230px;
+    margin: 0 6px;
+  }
+}
+</style>
