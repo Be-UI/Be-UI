4339c3c12cfa23ad8ca31e7a34241c53039df017
baiwusanyu <740132583@qq.com>
2023-02-08 23:19:31 +0800

    docs: add be-link document

---
+++ docs/examples/link/icon.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex">
+    <be-link
+        href="/"
+        content="underline"
+        type="primary"
+        underline
+        prevIcon="add"
+        nextIcon="loading"
+        target="_blank"/>
+  </div>
+</template>
---
+++ docs/examples/link/underline.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex">
+    <be-link href="/" content="underline" type="primary" :underline="true" target="_blank"/>
+  </div>
+</template>
